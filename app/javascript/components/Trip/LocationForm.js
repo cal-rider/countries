6aa15185563eaef122ea7ae73e6743d830b2a9bb
cal-rider <cal.rider3@gmail.com>
2021-02-28 18:44:43 -0500

    Wrapped up styling

--- app/javascript/components/Trip/LocationForm.js
+++ app/javascript/components/Trip/LocationForm.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+const LocationsContainer = styled.div`
+    text-align: center;
+    border-radius: 4px;
+    font-size: 18px;
+    padding: 40px 0 10px 0;
+    border:1px solid #e6e6e6;
+    background: #fff;
+`
 
 const LocationForm = (props) => {
     return (
-        <div className = "wrapper">
+        <LocationsContainer>
             <form onSubmit={props.handleSubmit}>
-                <div>wamt to add another location to the {props.attributes.name} trip?</div>
+                <div>Where else would you like to go on your "{props.attributes.name}" trip?</div>
                 <div className = "Field">
                     <input onChange={props.handleChange} value ={props.location.name} type="text" name="name" placeholder="Location Name"/>
                     <input onChange={props.handleChange} value ={props.location.description} type="text" name="description" placeholder="Description"/>
                 </div>
                 <button type="submit">Add Location</button>
             </form>
-        </div>
+        </LocationsContainer>
 
     )
 
