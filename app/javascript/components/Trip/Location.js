eb9070cbfba8b29fec1acb266b730068f1026ebf
cal-rider <cal.rider3@gmail.com>
2021-03-07 16:43:35 -0500

    more

--- app/javascript/components/Trip/Location.js
+++ app/javascript/components/Trip/Location.js
@@ -1,4 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Card = styled.div`
+    border: 1px solid rgba(0,0,0,0.1);
+    border-radius: 4px;
+    padding: 10px;
+    margin: 0 10px 10px 0;
+`
+const Title = styled.div`
+    padding: 10px 0 0 0;
+    font-size: 18px;
+`
+const Description = styled.div`
+    padding: 20px 0 0 0;
+    font-size: 12px;
+`
+
 
 const Location = (props) => {
     console.log(props.attributes)
@@ -7,11 +24,11 @@ const Location = (props) => {
     
 
     return(
-        <div className='card'>
-            <div className='name'>{name}</div>
-            <div className='description'>{description}</div>
+        <Card>
+            <Title>{name}</Title>
+            <Description>{description}</Description>
             
-        </div>
+        </Card>
     )
 
 }
